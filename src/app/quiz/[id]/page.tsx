8ec54b24917ad6868feb541cda14316b62d06e9e
Rupert Caingal <johnrupertcaingal@gmail.com>
2025-07-25 23:41:05 +0800

    added: [id] for quiz

---
+++ src/app/quiz/[id]/page.tsx
@@ -0,0 +1,12 @@
+import UnderDevelopment from '@/components/shared/underDevelopment'
+import React from 'react'
+
+const QuizPage = () => {
+  return (
+    <div>
+        <UnderDevelopment/>
+    </div>
+  )
+}
+
+export default QuizPage
